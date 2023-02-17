4acafc0fafc68f9b8871af1faaecb60933ad5d2a
Elena Zioga <elena@arrikto.com>
2023-02-17 15:40:00 +0200

    mwa(front): Format code
    Signed-off-by: Elena Zioga <elena@arrikto.com>

--- frontend/src/app/services/backend.service.spec.ts
+++ frontend/src/app/services/backend.service.spec.ts
@@ -4,13 +4,11 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 describe('BackendService', () => {
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule, MatSnackBarModule],
-      }).compileComponents();
-    }),
-  );
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, MatSnackBarModule],
+    }).compileComponents();
+  }));
   it('should be created', () => {
     const service: BackendService = TestBed.inject(BackendService);
     expect(service).toBeTruthy();
--- frontend/src/app/services/grafana.service.spec.ts
+++ frontend/src/app/services/grafana.service.spec.ts
@@ -5,13 +5,11 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { GrafanaService } from './grafana.service';
 
 describe('GrafanaService', () => {
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule, MatSnackBarModule],
-      }).compileComponents();
-    }),
-  );
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, MatSnackBarModule],
+    }).compileComponents();
+  }));
 
   it('should be created', () => {
     const service: GrafanaService = TestBed.inject(GrafanaService);

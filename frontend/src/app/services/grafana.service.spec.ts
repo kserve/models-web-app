e5880d994d830ee01b71146f89ccbf62f9675f17
Prateek Bala <prateekbala28@gmail.com>
2026-04-06 03:37:07 +0530

    test: fix Jest test configuration and reduce console errors
    Signed-off-by: Prateek Bala <prateekbala28@gmail.com>

--- frontend/src/app/services/grafana.service.spec.ts
+++ frontend/src/app/services/grafana.service.spec.ts
@@ -1,13 +1,26 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { SnackBarService } from 'kubeflow';
+import { of } from 'rxjs';
 
 import { GrafanaService } from './grafana.service';
+import { ConfigService } from './config.service';
 
 describe('GrafanaService', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, MatSnackBarModule],
+      providers: [
+        SnackBarService,
+        {
+          provide: ConfigService,
+          useValue: {
+            getConfig: () => of({ grafanaPrefix: '/grafana' }),
+          },
+        },
+        GrafanaService,
+      ],
     }).compileComponents();
   }));
 
